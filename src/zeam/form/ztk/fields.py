db3c163d11f981faacb32e409a46498f39b8d8e7
Sylvain Viollon <thefunny@gmail.com>
2009-10-10 16:14:05 +0200

    Register manually the adapters, to have them when the ZCML load.

--- src/zeam/form/ztk/fields.py
+++ src/zeam/form/ztk/fields.py
@@ -8,15 +8,17 @@ from zeam.form.ztk.interfaces import ISchemaField
 
 from zope.interface import Interface
 from zope.schema import interfaces as schema_interfaces
-from zope import schema
+from zope import schema, component
 import zope.interface.interfaces
 
 from grokcore import component as grok
 
 
-class SchemaFieldFactory(grok.Adapter):
+class SchemaFieldFactory(object):
     grok.implements(interfaces.IFieldFactory)
-    grok.context(zope.schema.interfaces.IField)
+
+    def __init__(self, context):
+        self.context = context
 
     def produce(self):
         interface = self.context.interface
@@ -25,17 +27,30 @@ class SchemaFieldFactory(grok.Adapter):
         yield interfaces.IField(self.context)
 
 
-class InterfaceSchemaFieldFactory(grok.Adapter):
+# We register it by hand to have the adapter available when loading ZCML.
+component.provideAdapter(
+    SchemaFieldFactory,
+    (zope.schema.interfaces.IField,))
+
+
+class InterfaceSchemaFieldFactory(object):
     grok.implements(interfaces.IFieldFactory)
-    grok.context(zope.interface.interfaces.IInterface)
+
+    def __init__(self, context):
+        self.context = context
 
     def produce(self):
         for name, field in schema.getFieldsInOrder(self.context):
             yield interfaces.IField(field)
 
 
-class SchemaField(Field, grok.Adapter):
-    grok.provides(interfaces.IField)
+# We register it by hand to have the adapter available when loading ZCML.
+component.provideAdapter(
+    InterfaceSchemaFieldFactory,
+    (zope.interface.interfaces.IInterface,))
+
+
+class SchemaField(Field):
     grok.implements(ISchemaField)
     grok.context(schema_interfaces.IField)
 
@@ -56,12 +71,21 @@ class SchemaField(Field, grok.Adapter):
             return self._field.fromUnicode(value)
         return value
 
-    def getContentValue(self, setting):
-        content = setting.getContent()
+    def setContentValue(self, value, content):
+        self._field.set(content, value)
+
+    def getContentValue(self, content):
         return self._field.get(content, NO_VALUE)
 
     def getDefaultValue(self):
         return self._field.default
+
+
+# We register it by hand to have the adapter available when loading ZCML.
+component.provideAdapter(
+    SchemaField,
+    (schema_interfaces.IField,),
+    interfaces.IField)
 
 
 class SchemaWidgetExtractor(WidgetExtractor):
--- src/zeam/form/ztk/interfaces.py
+++ src/zeam/form/ztk/interfaces.py
@@ -6,6 +6,10 @@ class ISchemaField(interfaces.IField):
     """This is a field for zope schema field.
     """
 
+    def setContentValue(content, value):
+        """Set the value on the content.
+        """
+
     def fromUnicode(value):
         """This convert a value from a unicode string (or not).
         """
