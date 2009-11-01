ee4bf21dfc76d5ab39d3b8f758b2224a20f95755
Sylvain Viollon <thefunny@gmail.com>
2009-11-01 22:35:50 +0100

    Fix broken tests (due to update)

--- src/zeam/form/ztk/ftests/forms/ztkform.py
+++ src/zeam/form/ztk/ftests/forms/ztkform.py
@@ -27,35 +27,12 @@ We can now lookup our form by the name of its class:
 
   >>> len(form.actions)
   1
-
   >>> len(form.fields)
   2
 
-And we can render it:
 
-  >>> print form()
-  <html>
-    <head>
-    </head>
-    <body>
-      <form action="http://127.0.0.1" method="post"
-            enctype="multipart/form-data">
-        <h1>People form</h1>
-        <p>Form to send people outerspace</p>
-        <div class="fields">
-            <label class="field-label" for="form-name">Person name</label><br />
-            <input id="form-name" name="form.name" value="" />
-            <label class="field-label" for="form-age">Person age</label><br />
-            <span class="field-description">Age in years</span><br />
-            <input id="form-age" name="form.age" value="" />
-        </div>
-        <div class="actions">
-            <input type="submit" id="form-send" name="form.send"
-                   value="Send" />
-        </div>
-      </form>
-    </body>
-  </html>
+Integration test
+----------------
 
 Let's try to take a browser and submit that form:
 
@@ -65,46 +42,26 @@ Let's try to take a browser and submit that form:
   >>> from zope.testbrowser.testing import Browser
   >>> browser = Browser()
   >>> browser.handleErrors = False
-  >>> browser.open('http://localhost/person/personform')
 
+We can access the form, fill it and submit it:
+
+  >>> browser.open('http://localhost/person/personform')
   >>> namefield = browser.getControl('Person name')
   >>> namefield
-  <Control name='form.name' type='text'>
+  <Control name='form.field.name' type='text'>
   >>> namefield.value = 'Arthur Sanderman'
 
   >>> agefield = browser.getControl('Person age')
   >>> agefield
-  <Control name='form.age' type='text'>
+  <Control name='form.field.age' type='text'>
   >>> agefield.value = '42'
 
   >>> action = browser.getControl('Send')
   >>> action
-  <SubmitControl name='form.send' type='submit'>
+  <SubmitControl name='form.action.send' type='submit'>
 
   >>> action.click()
-  >>> print browser.contents
-  <html>
-    <head>
-    </head>
-    <body>
-      <form action="http://localhost/person/personform"
-            method="post" enctype="multipart/form-data">
-        <p class="status-message">We sent Arthur Sanderman, age 42</p>
-        <h1>People form</h1>
-        <p>Form to send people outerspace</p>
-        <div class="fields">
-            <label class="field-label" for="form-name">Person name</label><br />
-            <input id="form-name" name="form.name" value="" />
-            <label class="field-label" for="form-age">Person age</label><br />
-            <span class="field-description">Age in years</span><br />
-            <input id="form-age" name="form.age" value="" />
-        </div>
-        <div class="actions">
-            <input type="submit" id="form-send" name="form.send"
-                   value="Send" />
-        </div>
-      </form>
-    </body>
-  </html>
+  >>> 'We sent Arthur Sanderman, age 42' in browser.contents
+  True
 
 """
