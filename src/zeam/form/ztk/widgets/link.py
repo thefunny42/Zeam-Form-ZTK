a597e2cda071557b8ac6c0f8582de218a2552db7
Sylvain Viollon <thefunny@gmail.com>
2009-11-11 20:45:26 +0100

    Restructuration default becomes widgets. Add a custom widget displaying the data as a link to the content.

---
+++ src/zeam/form/ztk/widgets/link.py
@@ -0,0 +1,18 @@
+
+from zeam.form.base.interfaces import IField
+from zeam.form.base.widgets import FieldWidget
+
+from zope.component import getMultiAdapter
+from zope.interface import Interface
+from zope.traversing.browser.interfaces import IAbsoluteURL
+
+from grokcore import component as grok
+
+
+class LinkFieldWidget(FieldWidget):
+    grok.adapts(IField, Interface, Interface)
+    grok.name('link')
+
+    def url(self):
+        content = self.form.getContent()
+        return getMultiAdapter((content, self.request), IAbsoluteURL)()
