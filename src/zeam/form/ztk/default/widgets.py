60d1a048c848acdf178d740cb23d7233291335a4
Sylvain Viollon <thefunny@gmail.com>
2009-11-11 09:35:08 +0100

    Add a Datetime display wiget. Widgets are registered for all composant (and not only IFormCanvas)

--- src/zeam/form/ztk/default/widgets.py
+++ src/zeam/form/ztk/default/widgets.py
@@ -1,7 +1,7 @@
 
-from zeam.form.base.widgets import FieldWidget, WidgetExtractor
+from zeam.form.base.widgets import FieldWidget, DisplayFieldWidget, \
+    WidgetExtractor
 from zeam.form.base.markers import NO_VALUE
-from zeam.form.base.interfaces import IFormCanvas
 from zeam.form.ztk.fields import SchemaField, registerSchemaField
 
 from zope.schema import interfaces as schema_interfaces
@@ -20,7 +20,7 @@ registerSchemaField(PasswordSchemaField, schema_interfaces.IPassword)
 
 
 class PasswordFieldWidget(FieldWidget):
-    grok.adapts(PasswordSchemaField, IFormCanvas, Interface)
+    grok.adapts(PasswordSchemaField, Interface, Interface)
 
 
 # Text widget
@@ -33,7 +33,7 @@ registerSchemaField(TextSchemaField, schema_interfaces.ISourceText)
 
 
 class TextFieldWidget(FieldWidget):
-    grok.adapts(TextSchemaField, IFormCanvas, Interface)
+    grok.adapts(TextSchemaField, Interface, Interface)
 
 
 # Checkbox widget
@@ -46,11 +46,11 @@ registerSchemaField(BooleanSchemaField, schema_interfaces.IBool)
 
 
 class CheckBoxFieldWidget(FieldWidget):
-    grok.adapts(BooleanSchemaField, IFormCanvas, Interface)
+    grok.adapts(BooleanSchemaField, Interface, Interface)
 
 
 class CheckBoxWidgetExtractor(WidgetExtractor):
-    grok.adapts(BooleanSchemaField, IFormCanvas, Interface)
+    grok.adapts(BooleanSchemaField, Interface, Interface)
 
     def extract(self):
         value, error = super(CheckBoxWidgetExtractor, self).extract()
@@ -59,4 +59,22 @@ class CheckBoxWidgetExtractor(WidgetExtractor):
         elif value == 'True':
             value = True
         return (value, error)
+
+# Datetime
+
+class DatetimeSchemaField(SchemaField):
+
+    def __init__(self, field):
+        super(DatetimeSchemaField, self).__init__(field)
+
+
+registerSchemaField(DatetimeSchemaField, schema_interfaces.IDatetime)
+
+
+class DatetimeDisplayFieldWidget(DisplayFieldWidget):
+    grok.adapts(DatetimeSchemaField, Interface, Interface)
+
+    def valueToUnicode(self, value):
+        formatter = self.request.locale.dates.getFormatter('dateTime')
+        return formatter.format(value)
 
