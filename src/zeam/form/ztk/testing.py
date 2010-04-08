0ced87fb12aa59951c01b1780d0473f350dfd45b
Sylvain Viollon <thefunny@gmail.com>
2010-04-08 14:53:24 +0200

    Be compatible with old ZCMLLayer.

--- src/zeam/form/ztk/testing.py
+++ src/zeam/form/ztk/testing.py
@@ -10,7 +10,7 @@ from grokcore.component import zcml
 ftesting_zcml = os.path.join(
     os.path.dirname(zeam.form.ztk.__file__), 'ftesting.zcml')
 FunctionalLayer = ZCMLLayer(
-    ftesting_zcml, __name__, 'FunctionalLayer', allow_teardown=False)
+    ftesting_zcml, __name__, 'FunctionalLayer')
 
 def setUp(test):
     FunctionalTestSetup().setUp()
