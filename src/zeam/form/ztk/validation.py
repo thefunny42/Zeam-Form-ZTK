ffdf0b247c66ed70ecbb3e77deab5333cbfecccc
Souheil CHELFOUH <trollfot@gmail.com>
2010-06-08 00:28:48 +0200

    Added a Data object, that behaves like a proxy, providing the data values

--- src/zeam/form/ztk/validation.py
+++ src/zeam/form/ztk/validation.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from zope.interface.interfaces import IMethod
+from zope.interface import directlyProvides, Invalid
+
+UNAVAILABLE = object()
+
+
+class Data(object):
+ 
+    def __init__(self, interface, data):
+        self.interface = interface
+        self.data = data
+        directlyProvides(self, interface)
+ 
+    def __getattr__(self, name):
+        try:
+            field = self.interface[name]
+        except KeyError:
+            raise AttributeError(name)
+
+        if IMethod.providedBy(field):
+            raise RuntimeError("Data value is not a schema field", name)
+
+        data = self.data
+        value = data.get(name, UNAVAILABLE)
+        
+        if value is UNAVAILABLE:
+            raise AttributeError(name)
+
+        return value
+
+
 class InvariantsValidation(object):
 
-    def __init__(self, *fields):
+    def __init__(self, fields):
         self.interfaces = []
         for field in fields:
             interface = field._field.interface
@@ -13,5 +44,10 @@ class InvariantsValidation(object):
     def validate(self, data):
         errors = []
         for interface in self.interfaces:
-            interface.validateInvariants(date, errors)
+            obj = Data(interface, data)
+            try:
+                interface.validateInvariants(obj, errors)
+            except Invalid:
+                pass # We continue to get a complete errors log.
+            
         return errors
