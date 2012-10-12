885adcc8a6939f0cc6a726b2b0987a1640a01481
Sylvain Viollon <thefunny@gmail.com>
2012-10-12 12:09:41 +0200

    Tag 1.3.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.3dev'
+version = '1.3'
 
 tests_require = [
     'zope.app.schema',
@@ -37,7 +37,7 @@ setup(name='zeam.form.ztk',
           'grokcore.component',
           'grokcore.view',
           'grokcore.security',
-          'grokcore.chameleon',
+          'grokcore.chameleon >= 1.0.3',
           'setuptools',
           'zeam.form.base >= 1.2.3',
           'zope.component',
