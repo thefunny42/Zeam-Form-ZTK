c9a370ad67a448c9b67777503cc3213cd6e13dd8
Sylvain Viollon <thefunny@gmail.com>
2013-12-20 12:04:04 +0100

    Update the javascript of the collection widget not to bind events on the document, but on the field container. This prevent to break the javascript if the events are intercepted between the field container and the document. Update the template to use buttons who doesn't submit for reordering the lines of the collection widget. Since reordering isn't supported without Javascript this is useless to submit the form for it.

--- src/zeam/form/ztk/widgets/static/collection.js
+++ src/zeam/form/ztk/widgets/static/collection.js
@@ -2,7 +2,7 @@
 // Requires: json-template.js and jQuery 1.7
 
 (function ($, jsontemplate){
-    var field_name_regexp = /(.*)\.field\.(\d+)$/;
+    var FIELD_NAME_REGEXP = /(.*)\.field\.(\d+)$/;
 
     var increment = function(value) {
         return (parseInt(value) + 1).toString();
@@ -23,15 +23,15 @@
             return undefined;
         };
 
-        var identifier = $node.attr('rel');
-        var template = new jsontemplate.Template(
-            $.trim($node.get(0).innerHTML), {
-                // For the moment this use an hijack version of
-                // json-template.
-                undefined_callable: function(name){
-                    return '{' + name + '}';
-                }
-            });
+        var identifier = $node.attr('rel'),
+            template = new jsontemplate.Template(
+                $.trim($node.get(0).innerHTML), {
+                    // For the moment this use an hijack version of
+                    // json-template.
+                    undefined_callable: function(name){
+                        return '{' + name + '}';
+                    }
+                });
 
         // Remove the template from the DOM.
         $node.remove();
@@ -50,7 +50,7 @@
 
                 // The line might contains other field-collection
                 data.find('div.field-collection').each(function (){
-                    prepare_field($(this));
+                    create_field($(this));
                 });
 
                 return data;
@@ -58,26 +58,10 @@
         };
     };
 
-    var prepare_field = function($field) {
-        if (!$field.is('.field-collection')) {
-            return;
-        };
-
-        var $container = $field.find('.field-collection-lines:first');
-        $field.data(
-            'template',
-            create_template($field.children('.field-collection-template')));
-
-        // Clear style on any existing buttons.
-        update_move_buttons(
-            $container.children('.field-collection-line:first'),
-            $container.children('.field-collection-line:last'));
-    };
-
-    var update_line_names = function(line, base_name, count) {
-        var selector_name = base_name + '.checked.';
-        var present_name = base_name + '.present.';
-        var field_name = base_name + '.field.';
+    var update_line_names = function($line, base_name, count) {
+        var selector_name = base_name + '.checked.',
+            present_name = base_name + '.present.',
+            field_name = base_name + '.field.';
 
         var rewriter = function () {
             var $input = $(this),
@@ -102,12 +86,12 @@
             };
         };
         // Rewrite name for input, textarea and select tags.
-        line.find('input').each(rewriter);
-        line.find('textarea').each(rewriter);
-        line.find('select').each(rewriter);
+        $line.find('input').each(rewriter);
+        $line.find('textarea').each(rewriter);
+        $line.find('select').each(rewriter);
 
         // Update the rel attribute on the file.
-        line.attr('rel', field_name + count);
+        $line.attr('rel', field_name + count);
     };
 
     var update_move_buttons = function($line_top, $line_bottom) {
@@ -128,20 +112,92 @@
         };
     };
 
-    $.extend($.fn, {
-        ZeamCollectionWidget: function () {
-            return $(this).each(function() { prepare_field($(this)) ;});
-        }
-    });
+    var create_field = function($field) {
+        if (!$field.is('.field-collection')) {
+            return;
+        };
 
-    $(document).ready(function (){
-        // Bind add buttons
-        $(document).on('click', 'form input.field-collection-add-line', function() {
-            var $field = $(this).closest('div.field-collection');
+        var $container = $field.find('.field-collection-lines:first'),
+            template = create_template($field.children('.field-collection-template'));
 
+
+        var move_line_down = function () {
+            var $line = $(this).closest('.field-collection-line'),
+                $next_line = $line.next();
+
+            if ($next_line.is('.field-collection-line')) {
+                var name_info = FIELD_NAME_REGEXP.exec($line.attr('rel')),
+                    base_name = name_info[1],
+                    count = name_info[2];
+
+                var next_name_info = FIELD_NAME_REGEXP.exec($next_line.attr('rel')),
+                    next_count = next_name_info[2];
+
+                $line.remove();
+                $line.insertAfter($next_line);
+                update_line_names($line, base_name, next_count);
+                update_line_names($next_line, base_name, count);
+                update_move_buttons($next_line, $line);
+            };
+            return false;
+        };
+
+        var move_line_up = function () {
+            var $line = $(this).closest('.field-collection-line'),
+                $previous_line = $line.prev();
+
+            if ($previous_line.is('.field-collection-line')) {
+                var name_info = FIELD_NAME_REGEXP.exec($line.attr('rel')),
+                    base_name = name_info[1],
+                    count = name_info[2];
+
+                var previous_name_info = FIELD_NAME_REGEXP.exec(
+                    $previous_line.attr('rel')),
+                    previous_count = previous_name_info[2];
+
+                $line.remove();
+                $line.insertBefore($previous_line);
+                update_line_names($line, base_name, previous_count);
+                update_line_names($previous_line, base_name, count);
+                update_move_buttons($line, $previous_line);
+            };
+            return false;
+        };
+
+        var remove_line =  function() {
+            var $selected = $container.children(
+                '.field-collection-line').children(
+                    '.line-actions').children(
+                        'input.field-collection-line-selector:checked');
+
+            $selected.each(function (){
+                var $line = $(this).closest('.field-collection-line'),
+                    $previous_line = $line.prev('.field-collection-line'),
+                    $next_line = $line.next('.field-collection-line');
+
+                $line.remove();
+                update_move_buttons($next_line, $previous_line);
+
+                var $lines = $container.find('.field-collection-line');
+
+                if (!$lines.length) {
+                    var $empty_message = $field.find('.field-collection-empty'),
+                        $header_message = $field.children('.field-collection-header'),
+                        $actions = $field.children('.multi-actions'),
+                        $remove_button = $actions.children('.field-collection-remove-line');
+
+                    $empty_message.slideDown();
+                    $header_message.slideUp();
+                    $remove_button.fadeOut();
+                };
+            });
+            return false;
+        };
+
+        var add_line = function() {
             // Clear the empty message
-            var $header_message = $field.children('.field-collection-header');
-            var $empty_message = $field.children('.field-collection-empty');
+            var $header_message = $field.children('.field-collection-header'),
+                $empty_message = $field.children('.field-collection-empty');
 
             if ($empty_message.is(':visible')) {
                 $header_message.slideDown();
@@ -149,18 +205,18 @@
             };
 
             // Display the remove button
-            var $actions = $field.children('.multi-actions');
-            var $remove_button = $actions.children('.field-collection-remove-line');
+            var $actions = $field.children('.multi-actions'),
+                $remove_button = $actions.children('.field-collection-remove-line');
             if (!$remove_button.is(':visible')) {
                 $remove_button.fadeIn();
             };
 
             // Add a line
-            var $counter = $field.children('input.field-collection-counter');
-            var identifier = $counter.val();
-            var $new_line = $field.data('template').render(identifier);
-            var $container = $field.find('.field-collection-lines:first');
-            var $previous_line = $container.children('.field-collection-line:last');
+            var $counter = $field.children('input.field-collection-counter'),
+                identifier = $counter.val(),
+                $new_line = template.render(identifier),
+                $container = $field.find('.field-collection-lines:first'),
+                $previous_line = $container.children('.field-collection-line:last');
 
             if (!$previous_line.length) {
                 $previous_line = $new_line;
@@ -170,87 +226,27 @@
             $counter.val(increment(identifier));
             $new_line.trigger('addline-zeamform');
             return false;
-        });
+        };
 
-        // Bind the remove button
-        $(document).on('click', 'form input.field-collection-remove-line', function() {
-            var field = $(this).closest('div.field-collection');
-            var container = field.find('.field-collection-lines:first');
-            var selected = container.children(
-                '.field-collection-line').children(
-                    '.line-actions').children(
-                        'input.field-collection-line-selector:checked');
+        // Bind events
+        $field.on('click', 'input.field-collection-add-line', add_line);
+        $field.on('click', 'input.field-collection-remove-line', remove_line);
+        $field.on('click', 'button.field-collection-move-up', move_line_up);
+        $field.on('click', 'button.field-collection-move-down', move_line_down);
 
-            selected.each(function (){
-                var line = $(this).closest('.field-collection-line');
-                var previous_line = line.prev('.field-collection-line');
-                var next_line = line.next('.field-collection-line');
+        // Clear style on any existing buttons.
+        update_move_buttons(
+            $container.children('.field-collection-line:first'),
+            $container.children('.field-collection-line:last'));
+    };
 
-                line.remove();
-                update_move_buttons(next_line, previous_line);
+    $.extend($.fn, {
+        ZeamCollectionWidget: function () {
+            return $(this).each(function() { create_field($(this));});
+        }
+    });
 
-                var lines = container.find('.field-collection-line');
-
-                if (!lines.length) {
-                    var empty_message = field.find('.field-collection-empty');
-                    var header_message = field.children('.field-collection-header');
-                    var actions = field.children('.multi-actions');
-                    var remove_button = actions.children('.field-collection-remove-line');
-
-                    empty_message.slideDown();
-                    header_message.slideUp();
-                    remove_button.fadeOut();
-                };
-            });
-            return false;
-        });
-
-        // Bind the up button
-        $(document).on('click', 'form button.field-collection-move-up', function () {
-            var button = $(this);
-            var line = button.closest('.field-collection-line');
-            var previous_line = line.prev();
-
-            if (previous_line.is('.field-collection-line')) {
-                var name_info = field_name_regexp.exec(line.attr('rel'));
-                var base_name = name_info[1];
-                var count = name_info[2];
-
-                var previous_name_info = field_name_regexp.exec(
-                    previous_line.attr('rel'));
-                var previous_count = previous_name_info[2];
-
-                line.remove();
-                line.insertBefore(previous_line);
-                update_line_names(line, base_name, previous_count);
-                update_line_names(previous_line, base_name, count);
-                update_move_buttons(line, previous_line);
-            };
-            return false;
-        });
-
-        // Bind the down button
-        $(document).on('click', 'form button.field-collection-move-down', function () {
-            var line = $(this).closest('.field-collection-line');
-            var next_line = line.next();
-
-            if (next_line.is('.field-collection-line')) {
-                var name_info = field_name_regexp.exec(line.attr('rel'));
-                var base_name = name_info[1];
-                var count = name_info[2];
-
-                var next_name_info = field_name_regexp.exec(next_line.attr('rel'));
-                var next_count = next_name_info[2];
-
-                line.remove();
-                line.insertAfter(next_line);
-                update_line_names(line, base_name, next_count);
-                update_line_names(next_line, base_name, count);
-                update_move_buttons(next_line, line);
-            };
-            return false;
-        });
-
+    $(document).ready(function (){
         // Load existing fields.
         $('form div.field-collection').ZeamCollectionWidget();
     });
